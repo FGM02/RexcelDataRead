67f61bde19abda6685bcf8cc98f2e69ddf75b061
FGM02 <109243559+FGM02@users.noreply.github.com>
2022-07-13 21:20:16 +0200

    Create DataRead.R

---
+++ DataRead.R
@@ -0,0 +1,28 @@
+#DataRead.R																						
+																						
+library(readxl)																						
+library(cellranger)																						
+																						
+address <- read_excel("TestTableAddress.xlsx")																						
+																						
+address[c("StartLetter", "StartNumber", "EndLetter", "EndNumber")] <- NA																						
+																						
+for (i in 1:nrow(address)) {																						
+	address$StartLetter[i] <- letter_to_num(regmatches(address$Range[i], gregexp("[[:alpha:]]+", address$Range[i]))[[1]][1])																					
+	address$StartNumber[i] <- regmatches(address$Range[i], gregexpr("[[:digit:]]+", address$Range[i]))[[1]][1]																					
+	address$EndLetter[i] <- letter_to_num(regmatches(address$Range[i], gregexp("[[:alpha:]]+", address$Range[i]))[[1]][2])																					
+	address$EndNumber[i] <- regmatches(address$Range[i], gregexpr("[[:digit:]]+", address$Range[i]))[[1]][2]																					
+}																						
+																						
+listOfObjs <- list()																						
+																						
+for (i in 1:nrow(address)) {																						
+	df <- read_excel("..", sheet = (address$Tab[i]),																					
+								cell_limits(c(address$StartNumber[i], address$StartLetter[i]), c(address$EndNumber[i], address$EndLetter[i])),														
+								col_names = F)														
+	listofObjs[[i]] <- df																					
+	names(listOfObjs)[i] <- address$Name[i]																					
+	assign(paste(address$Name[i]), df)																					
+}																						
+																						
+## Excel file with Name, Tab, Range columns. Blank cells for auto detection of table boundaries.	
